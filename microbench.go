ea5cd831c07ce81c215833e9b95b79dbe76e62ab
Leonhard Hofer <117044404+gitb8@users.noreply.github.com>
2023-02-14 23:28:24 +0100

    Add files via upload

---
+++ microbench.go
@@ -0,0 +1,83 @@
+package main
+
+// add packages for bench:
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+// add imports for bench:
+
+// set run repetitions:
+const RUNS int = 65536
+
+// add variables for bench:
+
+// var a int64 = 0
+
+func main() {
+	var start time.Time
+	var first time.Duration
+	var second time.Duration
+
+	// add local variables for bench:
+
+	var a int64 = 0
+
+	start = time.Now()
+	for i := 0; i < RUNS; i++ {
+		// 1st run
+
+		a++
+		// a += 1		// reverse test
+
+		// bench function
+		func1()
+		// func2()		// reverse test
+	}
+	first = time.Since(start)
+
+	// reset bench variables:
+
+	a = 0
+
+	// run garbage collector
+	runtime.GC()
+
+	start = time.Now()
+	for i := 0; i < RUNS; i++ {
+		// 2nd run
+
+		a += 1
+		// a++			// reverse test
+
+		// bench function
+		func2()
+		// func1()		// reverse test
+	}
+	second = time.Since(start)
+
+	// print result nanoseconds
+	// fmt.Println("1st run: ", float32(first)/float32(RUNS), " ns")
+	// fmt.Println("2nd run: ", float32(second)/float32(RUNS), " ns")
+
+	// print result microseconds
+	fmt.Println("1st run: ", float32(first)/float32(RUNS)/1000.0, " µs")
+	fmt.Println("2nd run: ", float32(second)/float32(RUNS)/1000.0, " µs")
+
+	// print result milliseconds
+	// fmt.Println("1st run: ", float32(first)/float32(RUNS)/1.0e6, " ms")
+	// fmt.Println("2nd run: ", float32(second)/float32(RUNS)/1.0e6, " ms")
+
+	// print result seconds
+	// fmt.Println("1st run: ", float32(first)/float32(RUNS)/1.0e9, " s")
+	// fmt.Println("2nd run: ", float32(second)/float32(RUNS)/1.0e9, " s")
+}
+
+// add functions for bench:
+
+func func1() {} // demo function
+
+func func2() {} // demo function
